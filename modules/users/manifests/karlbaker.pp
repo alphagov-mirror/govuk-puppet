7fc063276474ed0f07bf2ade918e16519357d8e7
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2019-12-10 13:38:53 +0000

    Update Karl's public SSH key
    This commit updates Karl's public SSH key.

--- modules/users/manifests/karlbaker.pp
+++ modules/users/manifests/karlbaker.pp
@@ -3,6 +3,6 @@ class users::karlbaker {
   govuk_user { 'karlbaker':
     fullname => 'Karl Baker',
     email    => 'karl.baker@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCo6AI0U+VFs2dkrwNDMKcU7j5lhSPFDuboQrsGyC1tv8aUBlCSfVDaPRPT20lYANN7tFgQD9mKXaMlUd9bCJJFs7L3CbE+/kSQCE85rfPFDaTMb3/WzYzwMjDD05pRtvdBwmkS6o8IFA4Yyd29qahYhnrO3jexBIZnNdM4nWZCac+nX/8bWckPOGWIR7fTNWoS8C8tioiUDqa/ZflzGqA0NKv7M0I1kwKqHt25FHaqZxnGmnKEC9QIUGbS4cC1cJQ2AO3NqJGPWhb39QrZwvv+Juh9rU3vuohDfx7Xm1Lh8NMVf3+c1vNTcK+DvaGGLZJ20JUBXlRRFFviLo9eaaf5fHIn5bM9aKFwxoPZtlj73FQpv04bUJf/LlbnGgLeW+B6Pl2w2qFp3u5p5NtvPEnLLPm4ljiPsJwl6vdmZy/xLc8/Ze0xyOeMaENm9MFK8BykgBqXqmEsSntvryP2fp1LgwDOt9ufLpF+yLq6hXl/JKYDJZiCTUjpwSbO/GRbsf2PCjzcwLxKIru3QtR9IZmiopeYRbDvH6Zofs/4M4mQVlBv15CKthVSTwwvcb9vOQyRa2uRW6FkY//g04+2yXpU742Cuh0CgNUd1fJ/vO3kCO0Fup/m3M0B7Vsr4NE5/0XdZSXOd5sF9oB4uVh9vE7apD0+hGbeQHzXz5cRNn52KQ== karl.alec.baker@gmail.com',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDHFbzqpfGmgWsnpaPG6kOw9yuB/jwV3ve+9OBqPfN3hSg92HSPdbQF2un37mv6LOli9mSAXioMm1GXaPyNMcCg2C/NFsehpVJjoUXkuho/xsui8b7WDCs3zjVi/ko7T1aH4wbZf1mUR3CRY88VeX2UiVyqG7pGbvMzTOG7uoJvvh/7gxPdAxB8pzfRTdrD65ohVkJggRDDT1CZjBcTg2dnZc5xsMnhDhMVtSLUZ/AqczhSFwfFCczy6wbgCTO7RtrRABd5bmr6WZ3LFWrLwF0pJ3wTrhMz94Ak3mVIOqJSGJKMRsdWVKRx0Ja6IUlKDzGuq3Bb8qFdSSI92Nd7eoaF cardno:000610162345',
   }
 }
