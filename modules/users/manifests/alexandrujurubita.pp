13c56f2c470833cf0eb2c3c7b89869d7be2da2dc
Alex <alex-ju@users.noreply.github.com>
2020-01-27 10:34:16 +0000

    Unmark alexandrujurubita as absent

--- modules/users/manifests/alexandrujurubita.pp
+++ modules/users/manifests/alexandrujurubita.pp
@@ -1,7 +1,6 @@
 # Creates the user alexandrujurubita
 class users::alexandrujurubita {
   govuk_user { 'alexandrujurubita':
-    ensure   => absent,
     fullname => 'Alexandru Jurubita',
     email    => 'alexandru.jurubita@digital.cabinet-office.gov.uk',
     ssh_key  => 'AAAAB3NzaC1yc2EAAAADAQABAAACAQDUkujeu8NelGw1Benz7+WmcF1zawCfnEfeEaInbM5/O+GIWZmauJ0k+dDERKkhV3Dp8EKQuvCLRcBXC/gw+alAb3f4OnbOrlDPU45rRqTsJ6Wh1v1qGBNGg8+6R5p+THwGxygkfKFNnk/hDXn0zjN4xtd4O5pTxN/SSGbHchzB0wj3HjFfuCqa2bxjTei7xS9HYaw0qKjmgB/DTSS9+uuzQnQ9dgiIOWFLMfxJxSkvF0j6bq4vTM05Cwz1CHuOeozdkvXkz6Nbe7KHqW47Y1MCfkp87DkvXxfGAllrNdeknObDTunBfu5UUOyF/wAg5bTS7dLYduplpqqzke2NxLLXpl1vWl0J03SfAIhG06jB+/aRgI3VKQjz2wpRRxdFCg0qbtCGSj32rP/QCype4AT8k7FDB2LSeiWndOLJbmPVfyHUO58UDC+dBGShcvM/M+Kg5iSLCdINMdkc7XHM7MDtjnwk7Zdn1+MRmY7wllqX0QuVMx7COX9O2clAHy+matuHXtBfxf/88HEj5CO+SUYxgYCMsKL4MI/MusGdBuTwle9SOEFEX3HgPuoCrYxZgXqiViLGDKbJII2ojNOjrwKNhkvsMrCXMW5hj7EzcCmAX1cYTGGktrIttxcCj5tHfobkvBYrdaczF1P9S60ENGefOziAiDPcXxxodDLxn1UFrw== alexandru.jurubita@digital.cabinet-office.gov.uk',
