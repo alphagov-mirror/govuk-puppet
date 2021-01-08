16feda9ddeb9b8ebea54e84f226af2bba3ba8845
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2021-01-08 18:52:37 +0000

    wip fix jenkins package test

--- modules/govuk_jenkins/spec/classes/jenkins__package_spec.rb
+++ modules/govuk_jenkins/spec/classes/jenkins__package_spec.rb
@@ -2,9 +2,15 @@ require_relative '../../../../spec_helper'
 
 describe 'govuk_jenkins::package', :type => :class do
   let(:params) {{
-    :govuk_python:apt_source:apt_mirror_hostname => 'apt.example.com',
     :version             => '1.554.2',
   }}
+
+  let(:pre_condition) {
+    class { 'govuk_python::apt_source':
+      apt_mirror_hostname => 'apt.example.com',
+    }
+
+  # let(:pre_condition) { '@filebeat::prospector {"foo": }' }
 
   let(:facts) {{
     :jenkins_plugins => 'fake_plugin 1.0'
